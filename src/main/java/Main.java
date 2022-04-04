6fc37d878e0b4c486c4d2648ddf6a268eb4c95f4
Anggelo Urso <anggelo.urso@gmail.com>
2022-04-04 15:49:57 -0400

    Adding instruction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,17 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         int a, b;
-        
         Scanner lScanner = new Scanner(System.in);
+        
         System.out.println("Ingrese un número: ");
         a = lScanner.nextInt();
 
         System.out.println("Ingrese otro número: ");
         b = lScanner.nextInt();
-
-        System.out.println(a);
-        System.out.println(b);
+        
+        // + suma
+        // - resta
+        // * multiplicacion
+        // / division (ojo con las divisiones por cero)
     }
 }
