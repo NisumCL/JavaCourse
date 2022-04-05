1c9e36c68bfa6bf671318b0ae7fd6378924e9e0b
Anggelo Urso <anggelo.urso@gmail.com>
2022-04-05 15:47:01 -0400

    Arreglos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,39 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 class Main {
     public static void main(String[] args) {
-        int a, b;
-        
-        Scanner lScanner = new Scanner(System.in);
-        System.out.println("Ingrese un número: ");
-        a = lScanner.nextInt();
+        int variable = 3;
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(arr[0]);
+                       // 0xaa01
+        exampleMethod(arr);
+                           // 0xab11
+        //System.out.println(variable);
+        //exampleMethod2(variable);
+        //System.out.println(variable);
+        System.out.println(arr[0]);
+        //System.out.println(arr[1]);
+        //System.out.println(arr[2]);
+        //System.out.println(arr[3]);
+        //System.out.println(arr[4]);
+    }
 
-        System.out.println("Ingrese otro número: ");
-        b = lScanner.nextInt();
+                                    // 0xaa01
+    public static void exampleMethod(int[] arr) {
+        // 0xaa01
+        // int[] arr2 = arr;
+        // 0xabc01
+        int[] arr2 = Arrays.copyOf(arr, arr.length);
+        arr2[0]++;
+        //int[] arr2 = Arrays.copyOf(arr, arr.length);
+        System.out.println(arr2[0] + arr2[1] + arr2[2]);
+    }
 
-        System.out.println(a);
-        System.out.println(b);
+                                      // 0xdf015
+    public static void exampleMethod2(int variable) {
+        variable = 25;
+        System.out.println(variable);
+        variable = 80;
+        System.out.println(variable);
     }
 }
